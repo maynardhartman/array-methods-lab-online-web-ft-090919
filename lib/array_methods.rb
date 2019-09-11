c847b348b36c4a033fb5366e5cae624c96b0f868
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-11 13:09:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,21 +3,21 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-
+  test = array.sort
 end
 
 def using_reverse(array)
-
+  test = array.reverse
 end
 
 def using_first(array)
-
+  test = array.first
 end
 
 def using_last(array)
-
+  test = array.last
 end
 
 def using_size(array)
-
+  test = array.size
 end
